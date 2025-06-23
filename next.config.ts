063430cabfa97407f7e4fa1143334f547f975691
norimoriyuki <msuryrsa@gmail.com>
2025-06-24 01:25:53 +0900

    dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
